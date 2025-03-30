52e3200c348cfababa0139bf8bb8022d4f9fccff
Muhammad Ali <123479909+spaqootech@users.noreply.github.com>
2025-03-30 11:43:10 +0500

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 
 int main() {
-	int array[5] = { 1,2,1,22,2 };
-	
+	int array[5] = { 1,2,1,1,1 };
+	int visited[5] = { false };
 	for (int i = 0; i < 5; i++) {
 		int count = 1;
-		for (int j = i + 1 ; j < 5; j++) {
+		for (int j = i + 1; j < 5; j++) {
 			if (array[i] == array[j]) {
-				count ++;
+				count++;
+				visited[j] = true;
 			}
 		}
-		if (count > 1) {
-		cout << array[i] << " has duplicat count of " << count << endl;
+		if (count > 1 && !visited[i]) {
+			cout << array[i] << " has duplicate count of " << count << endl;
 		}
 	}
 }
